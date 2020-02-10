685c4f08c4718415db1626016f97994336d7fc0e
yuhaochen0107@gmail.com <yuhaochen0107@gmail.com>
2020-02-10 16:21:54 -0500

    update handleUserRating

--- src/handleUserRating/index.js
+++ src/handleUserRating/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const uuidv4 = require('uuid/v4');
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-2' });
 
 exports.handler = async(event) => {
+
 	const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
+
 	if (!event || !event.body) {
 		return {
 			statusCode: 400,
@@ -16,63 +17,56 @@ exports.handler = async(event) => {
 		};
 	}
 	const body = JSON.parse(event.body);
-	if (!body.features || !Array.isArray(body.features) || body.features.length !== 23) {
+	for (const obj of body) {
+		if (!obj.id || (typeof obj.id) != 'string') {
+			return {
+				statusCode: 400,
+				headers: {
+					'Access-Control-Allow-Origin': '*'
+				},
+				body: JSON.stringify('Missing/Incorrect id!')
+			};
+		}
+		if (!obj.rating || (typeof obj.rating) !== 'number' || obj.rating < 0 || obj.rating > 100) {
+			return {
+				statusCode: 400,
+				headers: {
+					'Access-Control-Allow-Origin': '*'
+				},
+				body: JSON.stringify('Missing/Incorrect rating!')
+			};
+		}
+	}
+
+	let added = 0;
+	let failed = 0;
+	let err_message = '';
+	for (const obj of body) {
+		const params = {
+			TableName: 'schedule_data',
+			Key: { 'id': obj.id },
+			UpdateExpression: 'SET rating = list_append(rating, :val)',
+			ExpressionAttributeValues: { ':val': [obj.rating] }
+		};
+		try {
+			await docClient.update(params).promise();
+			added += 1;
+		}
+		catch (err) {
+			err_message += (err + '\n');
+			failed += 1;
+		}
+	}
+	if (added === 0) {
 		return {
-			statusCode: 400,
+			statusCode: 500,
 			headers: {
 				'Access-Control-Allow-Origin': '*'
 			},
-			body: JSON.stringify('Missing/Incorrect features!')
+			body: JSON.stringify(err_message)
 		};
 	}
-	if (!body.rating || (typeof body.rating) !== 'number' || body.rating < 0 || body.rating > 100) {
-		return {
-			statusCode: 400,
-			headers: {
-				'Access-Control-Allow-Origin': '*'
-			},
-			body: JSON.stringify('Missing/Incorrect rating!')
-		};
-	}
-
-	const entriesObj = {};
-
-	const featureHeaders = ['early_class',
-		'together_class',
-		'even_dist',
-		'mon_first_class_time',
-		'mon_last_class_time',
-		'mon_total_class_time',
-		'mon_num_classes',
-		'tue_first_class_time',
-		'tue_last_class_time',
-		'tue_total_class_time',
-		'tue_num_classes',
-		'wed_first_class_time',
-		'wed_last_class_time',
-		'wed_total_class_time',
-		'wed_num_classes',
-		'thu_first_class_time',
-		'thu_last_class_time',
-		'thu_total_class_time',
-		'thu_num_classes',
-		'fri_first_class_time',
-		'fri_last_class_time',
-		'fri_total_class_time',
-		'fri_num_classes'
-	];
-
-	entriesObj.id = uuidv4();
-	featureHeaders.forEach((x, i) => entriesObj[x] = body.features[i]);
-	entriesObj.rating = body.rating;
-
-	const params = {
-		TableName: 'features',
-		Item: entriesObj
-	};
-
-	try {
-		await docClient.put(params).promise();
+	else if (failed === 0) {
 		return {
 			statusCode: 200,
 			headers: {
@@ -80,13 +74,15 @@ exports.handler = async(event) => {
 			},
 			body: JSON.stringify('Success!')
 		};
-	} catch (err) {
+	}
+	else {
 		return {
-			statusCode: 500,
+			statusCode: 200,
 			headers: {
 				'Access-Control-Allow-Origin': '*'
 			},
-			body: JSON.stringify(`Unable to add item. Error: ${err}`)
+			body: JSON.stringify(added + ' rating updated.\n' + failed + ' failed to updated\n' + err_message)
 		};
 	}
+
 };
